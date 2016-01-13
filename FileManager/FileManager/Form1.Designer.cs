625eb93958ef4f36025346a7139bd3274a16d863
Igor Tsygin <Hakerok1995@gmail.com>
2016-01-13 17:13:09 +0700

    new

--- FileManager/FileManager/Form1.Designer.cs
+++ FileManager/FileManager/Form1.Designer.cs
@@ -30,58 +30,62 @@
         {
             this.components = new System.ComponentModel.Container();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
-            this.Replace = new System.Windows.Forms.Button();
+            this.ReplaceLeft = new System.Windows.Forms.Button();
             this.Exit = new System.Windows.Forms.Button();
-            this.Remove = new System.Windows.Forms.Button();
-            this.Copy = new System.Windows.Forms.Button();
+            this.RemoveLeft = new System.Windows.Forms.Button();
+            this.CopyLeft = new System.Windows.Forms.Button();
             this.RightKontrol = new FileManager.LvKontrol(this.components);
             this.LeftKontrol = new FileManager.LvKontrol(this.components);
+            this.CopyRight = new System.Windows.Forms.Button();
+            this.ReplaceRight = new System.Windows.Forms.Button();
+            this.RemoveRight = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Replace
+            // ReplaceLeft
             // 
-            this.Replace.Location = new System.Drawing.Point(57, 12);
-            this.Replace.Name = "Replace";
-            this.Replace.Size = new System.Drawing.Size(39, 32);
-            this.Replace.TabIndex = 1;
-            this.Replace.UseVisualStyleBackColor = true;
-            this.Replace.Click += new System.EventHandler(this.Replace_Click);
+            this.ReplaceLeft.Location = new System.Drawing.Point(57, 12);
+            this.ReplaceLeft.Name = "ReplaceLeft";
+            this.ReplaceLeft.Size = new System.Drawing.Size(39, 32);
+            this.ReplaceLeft.TabIndex = 1;
+            this.ReplaceLeft.UseVisualStyleBackColor = true;
+            this.ReplaceLeft.Click += new System.EventHandler(this.Replace_Click);
             // 
             // Exit
             // 
             this.Exit.BackgroundImage = global::FileManager.Properties.Resources.Exit;
             this.Exit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.Exit.Location = new System.Drawing.Point(147, 12);
+            this.Exit.Location = new System.Drawing.Point(1186, 12);
             this.Exit.Name = "Exit";
             this.Exit.Size = new System.Drawing.Size(39, 32);
             this.Exit.TabIndex = 3;
             this.Exit.UseVisualStyleBackColor = true;
             this.Exit.Click += new System.EventHandler(this.Exit_Click);
             // 
-            // Remove
+            // RemoveLeft
             // 
-            this.Remove.BackgroundImage = global::FileManager.Properties.Resources.Delete;
-            this.Remove.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.Remove.Location = new System.Drawing.Point(102, 12);
-            this.Remove.Name = "Remove";
-            this.Remove.Size = new System.Drawing.Size(39, 32);
-            this.Remove.TabIndex = 2;
-            this.Remove.UseVisualStyleBackColor = true;
-            this.Remove.Click += new System.EventHandler(this.Remove_Click);
+            this.RemoveLeft.BackgroundImage = global::FileManager.Properties.Resources.Delete;
+            this.RemoveLeft.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.RemoveLeft.Location = new System.Drawing.Point(102, 12);
+            this.RemoveLeft.Name = "RemoveLeft";
+            this.RemoveLeft.Size = new System.Drawing.Size(39, 32);
+            this.RemoveLeft.TabIndex = 2;
+            this.RemoveLeft.UseVisualStyleBackColor = true;
+            this.RemoveLeft.Click += new System.EventHandler(this.Remove_Click);
             // 
-            // Copy
+            // CopyLeft
             // 
-            this.Copy.BackgroundImage = global::FileManager.Properties.Resources.Сopy;
-            this.Copy.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.Copy.Location = new System.Drawing.Point(12, 12);
-            this.Copy.Name = "Copy";
-            this.Copy.Size = new System.Drawing.Size(39, 32);
-            this.Copy.TabIndex = 0;
-            this.Copy.UseVisualStyleBackColor = true;
-            this.Copy.Click += new System.EventHandler(this.Copy_Click);
+            this.CopyLeft.BackgroundImage = global::FileManager.Properties.Resources.Сopy;
+            this.CopyLeft.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.CopyLeft.Location = new System.Drawing.Point(12, 12);
+            this.CopyLeft.Name = "CopyLeft";
+            this.CopyLeft.Size = new System.Drawing.Size(39, 32);
+            this.CopyLeft.TabIndex = 0;
+            this.CopyLeft.UseVisualStyleBackColor = true;
+            this.CopyLeft.Click += new System.EventHandler(this.Copy_Click);
             // 
             // RightKontrol
             // 
+            
             this.RightKontrol.Location = new System.Drawing.Point(622, 50);
             this.RightKontrol.Name = "RightKontrol";
             this.RightKontrol.Size = new System.Drawing.Size(613, 432);
@@ -89,22 +93,57 @@
             // 
             // LeftKontrol
             // 
+           
             this.LeftKontrol.Location = new System.Drawing.Point(12, 50);
             this.LeftKontrol.Name = "LeftKontrol";
             this.LeftKontrol.Size = new System.Drawing.Size(604, 453);
             this.LeftKontrol.TabIndex = 4;
+            // 
+            // CopyRight
+            // 
+            this.CopyRight.BackgroundImage = global::FileManager.Properties.Resources.Сopy;
+            this.CopyRight.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.CopyRight.Location = new System.Drawing.Point(622, 12);
+            this.CopyRight.Name = "CopyRight";
+            this.CopyRight.Size = new System.Drawing.Size(39, 32);
+            this.CopyRight.TabIndex = 6;
+            this.CopyRight.UseVisualStyleBackColor = true;
+            this.CopyRight.Click += new System.EventHandler(this.CopyRight_Click);
+            // 
+            // ReplaceRight
+            // 
+            this.ReplaceRight.Location = new System.Drawing.Point(667, 12);
+            this.ReplaceRight.Name = "ReplaceRight";
+            this.ReplaceRight.Size = new System.Drawing.Size(39, 32);
+            this.ReplaceRight.TabIndex = 7;
+            this.ReplaceRight.UseVisualStyleBackColor = true;
+            this.ReplaceRight.Click += new System.EventHandler(this.ReplaceRight_Click);
+            // 
+            // RemoveRight
+            // 
+            this.RemoveRight.BackgroundImage = global::FileManager.Properties.Resources.Delete;
+            this.RemoveRight.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.RemoveRight.Location = new System.Drawing.Point(712, 12);
+            this.RemoveRight.Name = "RemoveRight";
+            this.RemoveRight.Size = new System.Drawing.Size(39, 32);
+            this.RemoveRight.TabIndex = 8;
+            this.RemoveRight.UseVisualStyleBackColor = true;
+            this.RemoveRight.Click += new System.EventHandler(this.RemoveRight_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1237, 526);
+            this.Controls.Add(this.RemoveRight);
+            this.Controls.Add(this.ReplaceRight);
+            this.Controls.Add(this.CopyRight);
             this.Controls.Add(this.RightKontrol);
             this.Controls.Add(this.LeftKontrol);
             this.Controls.Add(this.Exit);
-            this.Controls.Add(this.Remove);
-            this.Controls.Add(this.Replace);
-            this.Controls.Add(this.Copy);
+            this.Controls.Add(this.RemoveLeft);
+            this.Controls.Add(this.ReplaceLeft);
+            this.Controls.Add(this.CopyLeft);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
             this.Text = "Файловый менеджер";
@@ -115,13 +154,16 @@
 
         #endregion
 
-        private System.Windows.Forms.Button Copy;
+        private System.Windows.Forms.Button CopyLeft;
         private System.Windows.Forms.ToolTip toolTip1;
-        private System.Windows.Forms.Button Replace;
-        private System.Windows.Forms.Button Remove;
+        private System.Windows.Forms.Button ReplaceLeft;
+        private System.Windows.Forms.Button RemoveLeft;
         private System.Windows.Forms.Button Exit;
         private LvKontrol LeftKontrol;
         private LvKontrol RightKontrol;
+        private System.Windows.Forms.Button CopyRight;
+        private System.Windows.Forms.Button ReplaceRight;
+        private System.Windows.Forms.Button RemoveRight;
     }
 }
 
--- FileManager/FileManager/Form1.cs
+++ FileManager/FileManager/Form1.cs
@@ -9,9 +9,9 @@ namespace FileManager
         public Form1()
         {
             InitializeComponent();
-            toolTip1.SetToolTip(Copy, "Копировать (F3)");
-            toolTip1.SetToolTip(Replace, "Переместить (F4)");
-            toolTip1.SetToolTip(Remove, "Удалить (Delete)");
+            toolTip1.SetToolTip(CopyLeft, "Копировать (F3)");
+            toolTip1.SetToolTip(ReplaceLeft, "Переместить (F4)");
+            toolTip1.SetToolTip(RemoveLeft, "Удалить (Delete)");
             toolTip1.SetToolTip(Exit, "Выход (Alt+X)");
             KeyPreview = true;
         }
@@ -20,17 +20,17 @@ namespace FileManager
         {
             if (e.KeyCode == Keys.F3)
             {
-                Copy.PerformClick();
+                CopyLeft.PerformClick();
             }
 
             if (e.KeyCode == Keys.F4)
             {
-               Replace.PerformClick();
+               ReplaceLeft.PerformClick();
             }
 
             if (e.KeyCode == Keys.Delete)
             {
-                Remove.PerformClick();
+                RemoveLeft.PerformClick();
             }
 
             if (e.KeyCode == Keys.X && e.Alt)
@@ -55,20 +55,55 @@ namespace FileManager
                 AbstractFile qwe = new File(RightKontrol.ActiveDirectory.AbstractPath + @"\" + item.AbstractName);
                 item.AbstractCopy(qwe);
             }
+            KontrolsUpdates();
+        }
+
+        private void CopyRight_Click(object sender, EventArgs e)
+        {
+            foreach (var item1 in from item in RightKontrol.ActiveDirectory.DirectoriesList.Where(item => item.AbstractName == RightKontrol.ClickItem)
+                                  let copyToFolder = new Folder(LeftKontrol.ActiveDirectory.AbstractPath + @"\" + item.AbstractName)
+                                  from item1 in item.AbstractCopy(copyToFolder)
+                                  select item1)
+            {
+                // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+                item1.ToString();
+            }
+            foreach (var item in RightKontrol.ActiveDirectory.FilesList.Where(item => item.AbstractName == RightKontrol.ClickItem))
+            {
+                AbstractFile copyToFile = new File(LeftKontrol.ActiveDirectory.AbstractPath + @"\" + item.AbstractName);
+                item.AbstractCopy(copyToFile);
+            }
+            KontrolsUpdates();
         }
 
         private void Replace_Click(object sender, EventArgs e)
         {
             foreach (var item in LeftKontrol.ActiveDirectory.DirectoriesList.Where(item => item.AbstractName == LeftKontrol.ClickItem))
             {
-                var qwe = new Folder(RightKontrol.ActiveDirectory.AbstractPath + @"\" + item.AbstractName);
-                item.AbstractReplace(qwe);
+                var folderToReplace = new Folder(RightKontrol.ActiveDirectory.AbstractPath + @"\" + item.AbstractName);
+                item.AbstractReplace(folderToReplace);
             }
             foreach (var item in LeftKontrol.ActiveDirectory.FilesList.Where(item => item.AbstractName == LeftKontrol.ClickItem))
             {
-                var qwe = new Folder(RightKontrol.ActiveDirectory.AbstractPath);
-                item.AbstractReplace(qwe);
+                var fileToReplace = new Folder(RightKontrol.ActiveDirectory.AbstractPath);
+                item.AbstractReplace(fileToReplace);
             }
+            KontrolsUpdates();
+        }
+
+        private void ReplaceRight_Click(object sender, EventArgs e)
+        {
+            foreach (var item in RightKontrol.ActiveDirectory.DirectoriesList.Where(item => item.AbstractName == RightKontrol.ClickItem))
+            {
+                var folderToReplace = new Folder(LeftKontrol.ActiveDirectory.AbstractPath + @"\" + item.AbstractName);
+                item.AbstractReplace(folderToReplace);
+            }
+            foreach (var item in RightKontrol.ActiveDirectory.FilesList.Where(item => item.AbstractName == RightKontrol.ClickItem))
+            {
+                var fileToReplace = new Folder(LeftKontrol.ActiveDirectory.AbstractPath);
+                item.AbstractReplace(fileToReplace);
+            }
+            KontrolsUpdates();
         }
 
         private void Remove_Click(object sender, EventArgs e)
@@ -82,8 +117,22 @@ namespace FileManager
             {
                 item.AbstractRemove();
             }
+            KontrolsUpdates();
          }
 
+        private void RemoveRight_Click(object sender, EventArgs e)
+        {
+             foreach (var item in RightKontrol.ActiveDirectory.DirectoriesList.Where(item => item.AbstractName == RightKontrol.ClickItem))
+            {
+                item.AbstractRemove();
+            }
+             foreach (var item in RightKontrol.ActiveDirectory.FilesList.Where(item => item.AbstractName == RightKontrol.ClickItem))
+            {
+                item.AbstractRemove();
+            }
+            KontrolsUpdates();
+        }
+        
         private void Exit_Click(object sender, EventArgs e)
         {
             if (MessageBox.Show(@"Вы действительно хотите выйти", @"Выход", MessageBoxButtons.YesNo,
@@ -93,6 +142,15 @@ namespace FileManager
             }
         }
 
-       
+        protected void KontrolsUpdates()
+        {
+            LeftKontrol.ActiveDirectory = new Folder(LeftKontrol.ActiveDirectory.AbstractPath);
+            RightKontrol.ActiveDirectory = new Folder(RightKontrol.ActiveDirectory.AbstractPath);
+        }
+
+
+
+
+
     }
 }
